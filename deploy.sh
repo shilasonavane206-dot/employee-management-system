e48a741038b0a7c61b3eaa188d429cfcf97a4477
shila sonavane <shilasonavane206@gmail.com>
2026-07-26 16:09:49 +0530

    Update blue green deployment script

--- deploy.sh
+++ deploy.sh
@@ -5,36 +5,117 @@ echo "Employee Management System Deployment"
 echo "========================================"
 
 NGINX_CONFIG="/etc/nginx/sites-available/default"
+IMAGE_NAME="employee-management-system:latest"
+NETWORK_NAME="employee-management-system_ems_network"
 
-# Check which environment is currently active
+
+# Check current environment
+
 if grep -q "proxy_pass http://127.0.0.1:5001;" "$NGINX_CONFIG"; then
     CURRENT="blue"
     NEXT="green"
+    NEXT_PORT="5002"
+    CONTAINER_NAME="ems_green_app"
+
 elif grep -q "proxy_pass http://127.0.0.1:5002;" "$NGINX_CONFIG"; then
     CURRENT="green"
     NEXT="blue"
+    NEXT_PORT="5001"
+    CONTAINER_NAME="ems_blue_app"
+
 else
     echo "ERROR: Unable to determine active environment."
     exit 1
 fi
 
+
 echo "Current Environment : $CURRENT"
 echo "Next Environment    : $NEXT"
+echo "Next Port           : $NEXT_PORT"
 
-# Start next environment
+
+echo "Building Docker image..."
+
+docker build -t $IMAGE_NAME .
+
+
+echo "Removing old $CONTAINER_NAME if exists..."
+
+docker rm -f $CONTAINER_NAME 2>/dev/null || true
+
+
+echo "Starting $NEXT container..."
+
+
+docker run -d \
+  --name $CONTAINER_NAME \
+  --network $NETWORK_NAME \
+  -p $NEXT_PORT:5000 \
+  -e DB_HOST=db \
+  -e FLASK_ENV=production \
+  $IMAGE_NAME
+
+
+
+echo "Waiting for application startup..."
+
+sleep 10
+
+
+echo "Checking health..."
+
+if curl -f http://localhost:$NEXT_PORT/health; then
+
+    echo ""
+    echo "$NEXT environment is healthy"
+
+
+else
+
+    echo ""
+    echo "$NEXT environment failed health check"
+    docker logs $CONTAINER_NAME
+    exit 1
+
+fi
+
+
+
+echo "Switching Nginx traffic to $NEXT..."
+
 
 if [ "$NEXT" = "green" ]; then
 
-    echo "Starting Green container..."
+    sudo sed -i 's/5001/5002/g' $NGINX_CONFIG
 
-    docker build -t employee-management-system:latest .
+else
 
-    docker run -d \
-      --name ems_green_app \
-      --network employee-management-system_ems_network \
-      -p 5002:5000 \
-      -e DB_HOST=db \
-      -e FLASK_ENV=production \
-      employee-management-system:latest
+    sudo sed -i 's/5002/5001/g' $NGINX_CONFIG
 
 fi
+
+
+
+echo "Testing Nginx configuration..."
+
+sudo nginx -t
+
+
+if [ $? -eq 0 ]; then
+
+    sudo systemctl reload nginx
+
+    echo "Traffic switched successfully to $NEXT"
+
+else
+
+    echo "Nginx test failed"
+    exit 1
+
+fi
+
+
+
+echo "Deployment completed successfully"
+
+echo "Active Environment: $NEXT"
